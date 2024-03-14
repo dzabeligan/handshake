e7f975798af9b9f9e12cf8b004daeb212753b1a1
elijah.balogun <elijah.balogun@cyberpay.net.ng>
2024-03-14 10:20:00 +0100

    chore: Add example keyCheck usage

--- keyCheck.sh
+++ keyCheck.sh
@@ -2,3 +2,6 @@
 
 # Already built so we can comment out this line, uncomment if you haven't
 cc keyCheck.c des/*.c -o keyCheck
+
+./keyCheck -e 4821d7d8faf6e217be964222a37d2190 F2B8F619EC8651E4272E4B2F000BF462
+./keyCheck 85FB7FC4588332AB975E9E04409B897F 1600FF
